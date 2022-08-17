273ca6dbb14e3948f65d636857c4ecf96efea840
Robert B <robertb@lifesciencepublishers.com>
2022-08-17 13:52:52 -0600

    Add Deploy

--- server/src/app.js
+++ server/src/app.js
@@ -12,4 +12,8 @@ app.use(cors(
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..', 'public')))
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 module.exports = app
