37b9a38ec8f783ee24be7a0d0a44938e3371bf9a
RDBagwell <erbagwell@yahoo.com>
2022-08-29 12:25:59 -0600

    Add Morgan to server

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors(
         origin: 'http://localhost:3000'
     }
 ));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..', 'public')))
 
